d30d1d6daa19a492c89859b9c42e8a82d6281a2d
ericleonen <ericleonen@gmail.com>
2023-11-21 13:12:35 -0800

    initialize dummy generate api

---
+++ src/pages/api/generate.ts
@@ -0,0 +1,21 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type QuestionsData = {
+  questions: string[],
+  message?: string,
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<QuestionsData>
+) {
+  if (req.method === "POST") {
+    const questions = ["1", "2"];
+    res.status(200).json({ questions });
+  } else {
+    res.status(404).json({
+      questions: [],
+      message: "Not found"
+    });
+  }
+}
