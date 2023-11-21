489ee2482f1b50219994a30a2f48f843601173da
ericleonen <ericleonen@gmail.com>
2023-11-20 18:42:10 -0800

    adds interface sections

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,15 @@
+import Header from "@/components/Header";
+import QuestionsSection from "@/components/QuestionsSection";
+import TextSection from "@/components/TextSection";
+
 export default function App() {
   return (
-    <p>Hi there!</p>
+    <div className="flex h-screen w-screen">
+      <div className="flex flex-col">
+        <Header />
+        <TextSection />
+      </div>
+      <QuestionsSection />
+    </div>
   );
 }
