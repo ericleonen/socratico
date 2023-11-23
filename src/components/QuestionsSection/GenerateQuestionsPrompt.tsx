a72d502280ad690d15a08cf537987cccb4b2c1f7
ericleonen <ericleonen@gmail.com>
2023-11-23 11:06:18 -0800

    temporarily disable loader

--- src/components/QuestionsSection/GenerateQuestionsPrompt.tsx
+++ src/components/QuestionsSection/GenerateQuestionsPrompt.tsx
@@ -16,10 +16,7 @@ export default function GenerateQuestionsPrompt(
         <div className="flex flex-col justify-center h-full w-full px-10 items-center">{ 
             !loading ? (<>
                 <GenerateQuestionsButton 
-                    onClick={() => {
-                        generateQuestions();
-                        setLoading(true);
-                    }}
+                    onClick={generateQuestions}
                     disabled={disabled} 
                 />
                 <p className="text-gray-200/50 text-center mt-3">You don't have any questions yet. Let's generate some with AI!</p>
