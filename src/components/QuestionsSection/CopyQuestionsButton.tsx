a4f1e22f85ace6765f8df559c435aa49e6046229
ericleonen <ericleonen@gmail.com>
2023-11-22 18:49:38 -0800

    center copy button

--- src/components/QuestionsSection/CopyQuestionsButton.tsx
+++ src/components/QuestionsSection/CopyQuestionsButton.tsx
@@ -10,7 +10,7 @@ export default function CopyQuestionsButton({ onClick, disabled }: CopyQuestions
         <button
             onClick={onClick}
             disabled={disabled}
-            className="transition-all opacity-90 absolute right-6 text-gray-200/70 rounded-full p-[5px] flex justify-center items-center hover:bg-gray-200/10 hover:cursor-copy disabled:text-gray-200/20 disabled:hover:cursor-not-allowed disabled:hover:bg-transparent"
+            className="top-1/2 translate-y-[-50%] transition-all opacity-90 absolute right-6 text-gray-200/70 rounded-full p-[5px] flex justify-center items-center hover:bg-gray-200/10 hover:cursor-copy disabled:text-gray-200/20 disabled:hover:cursor-not-allowed disabled:hover:bg-transparent"
         >
             <ClipboardDocumentListIcon className="w-5 h-5 mr-[0.5px]" />
         </button>
